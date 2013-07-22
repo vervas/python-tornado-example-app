5a9db2b3f08732abde9189537a94f19f498ebac8
Dimitris Verraros <vervas.g@gmail.com>
2013-07-22 11:12:44 +0200

    Structure changes - introduce templates - update requirements

--- app.py
+++ app.py
@@ -1,26 +1,28 @@
-from tornado.options import define, options
+from tornado.options import define, options, parse_command_line
 import tornado.ioloop
 import tornado.web
 import tornado.httpserver
+import os
+
+define("port", default=8888, help="Port to listen on", type=int)
 
 
 class MainHandler(tornado.web.RequestHandler):
     @tornado.web.asynchronous
     def get(self):
-        self._async_callback()
-
-    def _async_callback(self):
-        self.write("Hello, world!")
-        self.finish()
-
-app = tornado.web.Application([
-    (r"/", MainHandler),
-])
-
-define("port", default="5555", help="Port to listen on")
+        self.render('hello.html', app_type='Tornado')
 
 if __name__ == "__main__":
-    tornado.options.parse_command_line()
+    parse_command_line()
+    app = tornado.web.Application(
+        [
+            (r"/", MainHandler),
+        ],
+        template_path=os.path.join(os.path.dirname(__file__), "templates"),
+        static_path=os.path.join(os.path.dirname(__file__), "static"),
+        xsrf_cookies=True,
+    )
+
     server = tornado.httpserver.HTTPServer(app)
     server.bind(options.port)
     # autodetect cpu cores and fork one process per core
